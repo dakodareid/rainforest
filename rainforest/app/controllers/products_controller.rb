8f6108b4a0dec933aa1072637cebe2eee5d2e152
dakodareid <YOUR EMAIL ADDRESSdakodareid@gmail.com>
2014-11-10 17:45:22 -0500

    form added for edit. titles edited for edit view as well as show

--- rainforest/app/controllers/products_controller.rb
+++ rainforest/app/controllers/products_controller.rb
@@ -28,7 +28,7 @@ class ProductsController < ApplicationController
   def update
   	@product = Product.find(params[:id])
 
-  	if @product.update_attributes(product_name)
+  	if @product.update_attributes(product_params)
   		redirect_to product_path(@product)
   	else
   		render :edit
