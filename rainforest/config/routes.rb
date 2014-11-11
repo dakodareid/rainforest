a12d4db0906685d67788df78f13a611facb72820
dakodareid <YOUR EMAIL ADDRESSdakodareid@gmail.com>
2014-11-10 23:41:25 -0500

    new session form added to view/sessions/new

--- rainforest/config/routes.rb
+++ rainforest/config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
   # get 'users/new'
 
   # get 'users/create'
@@ -6,6 +12,7 @@ Rails.application.routes.draw do
   root 'products#index'
   resources :products
   resources :users, :only => [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
